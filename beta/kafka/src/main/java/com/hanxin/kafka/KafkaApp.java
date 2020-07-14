7f5640433edd4a555c926451d0758808a82fa3d2
yumindouzi <810238922@qq.com>
2020-07-14 23:18:45 +0800

    kafka模块 1.简单生产者消费者 2.带回调函数的生产者 3.自定义分区 4.带事务的生产者
    1、指定topic、partition、offset消费
    2、批量消费
    3、ConsumerAwareListenerErrorHandler 异常处理器
    4、消息过滤器
    5、消息转发
    6、定时启动、停止监听器

---
+++ beta/kafka/src/main/java/com/hanxin/kafka/KafkaApp.java
@@ -0,0 +1,18 @@
+package com.hanxin.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: hanxin
+ * @Date: 2020/7/14 21:48
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class KafkaApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaApp.class);
+  }
+
+}
---
+++ beta/kafka/src/main/java/com/hanxin/kafka/producer/KafkaProducer.java
@@ -0,0 +1,78 @@
+package com.hanxin.kafka.producer;
+
+import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: hanxin
+ * @Date: 2020/7/14 22:27
+ * @Version 1.0
+ */
+@RestController
+@Slf4j
+public class KafkaProducer {
+  @Resource
+  private KafkaTemplate<String, Object> kafkaTemplate;
+
+  // 发送消息
+  @GetMapping("/kafka/normal/{message}")
+  public void sendMessage1(@PathVariable("message") String normalMessage) {
+    kafkaTemplate.send("topic1", normalMessage);
+    log.info("normal message send success ----> {}", normalMessage);
+  }
+
+  @GetMapping("/kafka/callbackOne/{message}")
+  public void sendMessage2(@PathVariable("message") String callbackMessage) {
+    kafkaTemplate.send("topic1", callbackMessage).addCallback(success -> {
+      // 消息发送到的topic
+      String topic = success.getRecordMetadata().topic();
+      // 消息发送到的分区
+      int partition = success.getRecordMetadata().partition();
+      // 消息在分区内的offset
+      long offset = success.getRecordMetadata().offset();
+      System.out.println("发送消息成功:" + topic + "-" + partition + "-" + offset);
+    }, failure -> {
+      System.out.println("发送消息失败:" + failure.getMessage());
+    });
+  }
+
+  @GetMapping("/kafka/callbackTwo/{message}")
+  public void sendMessage3(@PathVariable("message") String callbackMessage) {
+    kafkaTemplate.send("topic1", callbackMessage).addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+      @Override
+      public void onFailure(Throwable ex) {
+        System.out.println("发送消息失败："+ex.getMessage());
+      }
+
+      @Override
+      public void onSuccess(SendResult<String, Object> result) {
+        System.out.println("发送消息成功：" + result.getRecordMetadata().topic() + "-"
+            + result.getRecordMetadata().partition() + "-" + result.getRecordMetadata().offset());
+      }
+    });
+  }
+
+  @GetMapping("/kafka/send")
+  public void sendMessage7(){
+    // 不声明事务：后面报错但前面消息已经发送成功了
+    kafkaTemplate.send("topic1","test executeInTransaction");
+    throw new RuntimeException("fail");
+  }
+
+  @GetMapping("/kafka/transaction")
+  public void sendMessage8(){
+    // 声明事务：后面报错消息不会发出去
+    kafkaTemplate.executeInTransaction(operations -> {
+      operations.send("topic1","test executeInTransaction");
+      throw new RuntimeException("fail");
+    });
+  }
+
+}
